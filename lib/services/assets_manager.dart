6d976da84b11cd8c8e44a5ab797e5c1ec47a68c0
Don-sunny <donsunny544@gmail.com>
2023-08-18 00:03:16 +0530

    base ui completed

---
+++ lib/services/assets_manager.dart
@@ -0,0 +1,6 @@
+class AssetManager {
+  static String imagePath = "assets/images";
+  static String userImagePath = "$imagePath/person.png";
+  static String botImage = "$imagePath/chat_logo.png";
+  static String chatLogo = "$imagePath/openai_logo.jpg";
+}
